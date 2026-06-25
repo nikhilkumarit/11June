611e6bb9303648b9942ef4a3f2fc9f4c16ddb3f1
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-06-25 22:17:25 +0530

    opps

--- opps.java
+++ opps.java
@@ -3,6 +3,12 @@ class Student {
     int age;
     int marks;
 
+    Student(String name, int age, int marks){
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
     void display() {
         System.out.println("Student name is " + name);
         System.out.println("Student age is : " + age);
@@ -21,17 +27,9 @@ class Student {
 public class opps {
 
     public static void main(String[] args) {
-        student s1 = new student();
-        s1.name = "Nikhil";
-        s1.age = 19;
-        s1.marks = 95;
 
-        student s2 = new student();
-        s2.name = "Rahul";
-        s2.age = 20;
-        s2.marks = 89;
-
-        
+        Student s1 = new Student("Nikhil", 19, 98);
+        Student s2 = new Student("Aman", 21, 87);
         s1.display();
         s2.display();
         
