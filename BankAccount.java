3394b33801e9cf5c7573c0ddcec49b35663c9df0
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-06-27 10:31:45 +0530

    Encapsulation-learning

--- BankAccount.java
+++ BankAccount.java
@@ -1,36 +1,37 @@
 public class BankAccount {
-    String owner;
-    double balance;
+//     String owner;
+//     double balance;
 
-    BankAccount(String owner, double balance) {
-        this.owner = owner;
-        this.balance = balance;
-    }
+//     BankAccount(String owner, double balance) {
+//         this.owner = owner;
+//         this.balance = balance;
+//     }
 
-    void deposit(double amount) {
-    balance += amount;
-    System.out.println(amount + " deposited.");
-}
+//     void deposit(double amount) {
+//     balance += amount;
+//     System.out.println(amount + " deposited.");
+// }
 
-    void withdraw(double amount) {
-        if (amount <= balance) {
-            balance = balance - amount;
-            System.out.println(amount + " withdraw.");
-        } else {
-            System.out.println("Balance is low.");
-        }
-    }
+//     void withdraw(double amount) {
+//         if (amount <= balance) {
+//             balance = balance - amount;
+//             System.out.println(amount + " withdraw.");
+//         } else {
+//             System.out.println("Balance is low.");
+//         }
+//     }
 
     
-    void checkbalance() {
-        System.out.println("Balance : " + balance);
-    }
+//     void checkbalance() {
+//         System.out.println("Balance : " + balance);
+//     }
     
     
-    public static void main(String[] args) {
-        BankAccount acc1 = new BankAccount("Nikhil", 50000);
-        acc1.deposit(10000);
-        acc1.withdraw(5000);
-        acc1.checkbalance();
-    }
+//     public static void main(String[] args) {
+//         BankAccount acc1 = new BankAccount("Nikhil", 50000);
+//         acc1.deposit(10000);
+//         acc1.withdraw(5000);
+//         acc1.checkbalance();
+//     }
+    
 }
---
+++ Person.java
@@ -0,0 +1,39 @@
+class Person {
+    private String name;
+    private int age;
+
+    // Constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Age is not vaild.");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Nikhil", 19);
+
+        p1.setName("Rahul");
+        p1.setAge(21);
+        System.out.println("Name is : " + p1.getName());
+        System.out.println("Age is : " + p1.getAge());
+    }
+}
