a8c2d61f9acc20af4ce229db4eef11709b247a53
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-06-28 22:24:57 +0530

    java Employee program

--- Employee.java
+++ Employee.java
@@ -3,35 +3,42 @@ public class Employee {
     private double salary;
     private String department;
 
-    Employee(String name,double salary,String department){
+    Employee(String name, double salary, String department) {
         this.name = name;
         this.salary = salary;
         this.department = department;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public double getSalary(){
+
+    public double getSalary() {
         return salary;
     }
-    public String getDepartment(){
+
+    public String getDepartment() {
         return department;
     }
-    public void setSalary(double salary){
-        if(salary < 0){
+
+    public void setSalary(double salary) {
+        if (salary < 0) {
             System.out.println("Salary is not valid.");
-        }
-        else{
+        } else {
             this.salary = salary;
         }
     }
-    void display(){
-        System.out.println("Name : "+name);
-        System.out.println("Salary : "+salary);
-        System.out.println("Department : "+department);
+
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Salary : " + salary);
+        System.out.println("Department : " + department);
     }
-    public static void main(String[] args){
-        Employee em1 = new Employee("Nikhil",50000,"IT");
-        em1.setSalary(60000); 
+
+    public static void main(String[] args) {
+        Employee em1 = new Employee("Nikhil", 50000, "IT");
+        em1.display();
+        em1.setSalary(60000);
+        em1.display();
     }
 }
