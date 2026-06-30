468b631bc850ade6bfb240cc9247f835464257df
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-06-30 11:37:11 +0530

    Encapsulation -- Program

--- Phone.java
+++ Phone.java
@@ -17,5 +17,24 @@ public class Phone {
     public int getBatteryLevel(){
         return battery;
     }
-    
+    public void display(){
+        System.out.println("\nBrand : "+brand);
+        System.out.println("Price : "+price);
+        System.out.println("BatteryLevel : "+battery);
+        System.out.println();
+    }
+    public void setBatterlevel(int battery){
+        if(battery >= 0 && battery <= 100){
+            this.battery = battery;
+        }
+        else{
+            System.out.println("Batterlevel is not vaild.");
+        }
+    }
+    public static void main(String[] args) {
+        Phone p1 = new Phone("Apple",150000,99);
+        p1.display();
+        p1.setBatterlevel(100);
+        p1.display();
+    }
 }
