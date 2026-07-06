d48f5accffb1f625b45804b811eeaa2f5d2a217d
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-07-06 22:33:28 +0530

    OPPS

--- sixteen.java
+++ sixteen.java
@@ -1,19 +1,18 @@
 public class sixteen {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         // String Palindrome check.
 
         String str = "madam";
         String reverse = "";
-        for(int i = str.length()-1;i >= 0;i--){
+        for (int i = str.length() - 1; i >= 0; i--) {
             reverse = reverse + str.charAt(i);
             System.out.println(reverse);
         }
 
-        if(reverse.equals(str)){
-            System.out.println("String is palindrome : "+str);
-        }
-        else{
+        if (reverse.equals(str)) {
+            System.out.println("String is palindrome : " + str);
+        } else {
             System.out.println("String is not palindrome.");
         }
     }
