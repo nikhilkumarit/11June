51823ecb1ff20d0f848be5173e1b87f0f26a4db2
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-07-06 22:32:05 +0530

    OPPS

--- Phone.java
+++ Phone.java
@@ -2,37 +2,42 @@ public class Phone {
     private String brand;
     private double price;
     private int battery;
-    
-    Phone(String brand, double price, int battery){
+
+    Phone(String brand, double price, int battery) {
         this.brand = brand;
         this.price = price;
         this.battery = battery;
     }
-    public String getBrand(){
+
+    public String getBrand() {
         return brand;
     }
-    public double getPrice(){
+
+    public double getPrice() {
         return price;
     }
-    public int getBatteryLevel(){
+
+    public int getBatteryLevel() {
         return battery;
     }
-    public void display(){
-        System.out.println("\nBrand : "+brand);
-        System.out.println("Price : "+price);
-        System.out.println("BatteryLevel : "+battery);
+
+    public void display() {
+        System.out.println("\nBrand : " + brand);
+        System.out.println("Price : " + price);
+        System.out.println("BatteryLevel : " + battery);
         System.out.println();
     }
-    public void setBatterlevel(int battery){
-        if(battery >= 0 && battery <= 100){
+
+    public void setBatterlevel(int battery) {
+        if (battery >= 0 && battery <= 100) {
             this.battery = battery;
-        }
-        else{
+        } else {
             System.out.println("Batterlevel is not vaild.");
         }
     }
+
     public static void main(String[] args) {
-        Phone p1 = new Phone("Apple",150000,99);
+        Phone p1 = new Phone("Apple", 150000, 99);
         p1.display();
         p1.setBatterlevel(100);
         p1.display();
--- function.java
+++ function.java
@@ -1,126 +1,127 @@
 public class function {
     // Q.5
     // public static int factorial(int a){
-    //     int fact = 1;
-    //     for(int i = a;i > 0;i--){
-    //          fact = fact * i;
-    //     }
-    //     return fact;
+    // int fact = 1;
+    // for(int i = a;i > 0;i--){
+    // fact = fact * i;
+    // }
+    // return fact;
     // }
     // public static void main(String[] args) {
-    //     int result = factorial(5);
-    //     System.out.println("Factorail of is : "+result);
+    // int result = factorial(5);
+    // System.out.println("Factorail of is : "+result);
     // }
 
     // Q.6
 
     // public static int countVowels(String str){
-    //     int count = 0;
-    //     for(int i = 0;i < str.length();i++){
-    //         char ch = Character.toLowerCase(str.charAt(i));
-        
-    //         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-    //             count++;
-    //         }
-    //     }
-    //     return count;
+    // int count = 0;
+    // for(int i = 0;i < str.length();i++){
+    // char ch = Character.toLowerCase(str.charAt(i));
+
+    // if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+    // count++;
+    // }
+    // }
+    // return count;
     // }
     // public static void main(String[] args) {
-    //     int result = countVowels("HELLO WORLD");
-    //     System.out.println(+result);
+    // int result = countVowels("HELLO WORLD");
+    // System.out.println(+result);
     // }
 
     // Q.7
     // public static int area(int side){
-    //     return side * side;
+    // return side * side;
     // }
     // public static int area(int length,int width){
-    //     return length * width;
+    // return length * width;
     // }
     // public static void main(String[] args) {
-    //     int result1 = area(10);
-    //     System.out.println("Area of square is : "+result1);
+    // int result1 = area(10);
+    // System.out.println("Area of square is : "+result1);
 
-    //     int result2 = area(10, 20);
-    //     System.out.println("Area of rectangle is : "+result2);
+    // int result2 = area(10, 20);
+    // System.out.println("Area of rectangle is : "+result2);
     // }
 
     // Q.8
 
     // public static boolean isPrime(int num){
-    //     for(int i = 2; i < num; i++){
-    //         if(num % i == 0){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
+    // for(int i = 2; i < num; i++){
+    // if(num % i == 0){
+    // return false;
+    // }
+    // }
+    // return true;
     // }
     // public static void main(String[] args) {
-    //     boolean result = isPrime(7);
-    //     System.out.println(result);
+    // boolean result = isPrime(7);
+    // System.out.println(result);
     // }
 
     // Q.9
 
     // public static int reverseNumber(int num){
-    //     int digit = 0;
-    //     int result = 0;
-    //     while(num > 0){
-    //         digit = num % 10;
-    //         num = num / 10;
-    //         result = result * 10 + digit;
-    //     }
-    //     return result;
-    // } 
+    // int digit = 0;
+    // int result = 0;
+    // while(num > 0){
+    // digit = num % 10;
+    // num = num / 10;
+    // result = result * 10 + digit;
+    // }
+    // return result;
+    // }
     // public static void main(String[] args) {
-    //     int finalresult = reverseNumber(1234);
-    //     System.out.println("Reverse number is : "+finalresult);
+    // int finalresult = reverseNumber(1234);
+    // System.out.println("Reverse number is : "+finalresult);
     // }
 
     // Q.10
 
     // public static void fibonacci(int num){
-    //     int a = 0;
-    //     int b = 1;
-    //     int c;
-    //     System.out.print(+a+ " " +b+ " ");
+    // int a = 0;
+    // int b = 1;
+    // int c;
+    // System.out.print(+a+ " " +b+ " ");
 
-    //     for(int i = 2;i < num;i++){
-    //         c = a + b;
-    //         System.out.print(c+ " ");
-    //         a = b;
-    //         b = c;
-    //     }
+    // for(int i = 2;i < num;i++){
+    // c = a + b;
+    // System.out.print(c+ " ");
+    // a = b;
+    // b = c;
+    // }
     // }
     // public static void main(String[] args) {
-    //     fibonacci(7);
-        
+    // fibonacci(7);
+
     // }
 
     // Recursion
 
     // public static int sumofnNumber(int num){
-    //     if(num == 1){
-    //         return 1;
-    //     }
-    //     return num + sumofnNumber(num - 1);
+    // if(num == 1){
+    // return 1;
+    // }
+    // return num + sumofnNumber(num - 1);
     // }
     // public static void main(String[] args) {
-    //     int result = sumofnNumber(5);
-    //     System.out.println(result);
+    // int result = sumofnNumber(5);
+    // System.out.println(result);
     // }
 
     // Recursion se Fibonacci!
 
-    public static int fibonacci(int num){
-        if(num == 0){
+    public static int fibonacci(int num) {
+        if (num == 0) {
             return 0;
         }
-        if(num == 1){
+        if (num == 1) {
             return 1;
         }
         return fibonacci(num - 1) + fibonacci(num - 2);
     }
+
     public static void main(String[] args) {
         int result = fibonacci(5);
         System.out.println(result);
