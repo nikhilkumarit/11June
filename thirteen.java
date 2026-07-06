ff44ef30fc7e8fe3799d0ad0c3554e865b01d92f
nikhilkumarit <nikhilkumarit01@gmail.com>
2026-07-06 22:33:38 +0530

    OPPS

--- thirteen.java
+++ thirteen.java
@@ -1,53 +1,54 @@
 import java.util.Scanner;
+
 public class thirteen {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         // int arr [] = {10,20,30,40,50};
         // int sum = 0;
         // int max = arr[0];
         // for(int i = 0;i <= arr.length-1;i++){
-        //     sum = sum + arr[i];
-        //     if(arr[i] > max){
-        //         max = arr[i];
-        //     }
+        // sum = sum + arr[i];
+        // if(arr[i] > max){
+        // max = arr[i];
+        // }
         // }
         // System.out.println("Sum is : "+sum);
         // System.out.println("Max number is : "+max);
 
-        //Reverse print array.
+        // Reverse print array.
 
         // int arr[] = {1,2,3,4,5};
         // for(int i = arr.length-1;i >= 0;i--){
-        //     System.out.println(arr[i]);
+        // System.out.println(arr[i]);
         // }
 
-        //2D-Array
-        // Ek array banao aur Scanner se ek number input lo. Check karo ki wo number array mein hai ya nahi ("Found" / "Not Found" print karo).
+        // 2D-Array
+        // Ek array banao aur Scanner se ek number input lo. Check karo ki wo number
+        // array mein hai ya nahi ("Found" / "Not Found" print karo).
 
-        int [][] nums = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9} 
+        int[][] nums = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
         System.out.print("Enter a number : ");
         int num = sc.nextInt();
 
         boolean found = false;
-        for(int i = 0;i < nums.length;i++){
-            for(int j = 0;j < nums.length;j++){
-                System.out.print(nums[i][j]+" ");
-                if(num == nums[i][j]){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                System.out.print(nums[i][j] + " ");
+                if (num == nums[i][j]) {
                     found = true;
                 }
             }
             System.out.println();
         }
-        if(found){
+        if (found) {
             System.out.println("Found");
-        }
-        else{
+        } else {
             System.out.println("Not found");
         }
-        
+
     }
-}    
+}
--- twelve.java
+++ twelve.java
@@ -42,19 +42,19 @@ public class twelve {
         // }
 
         // Pattern print.
-        //    *
-        //   * *
-        //  * * *
+        // *
+        // * *
+        // * * *
         // * * * *
 
-        for(int i = 1;i <= 4;i++){
-            for(int s = 4;s >= i;s--)
+        for (int i = 1; i <= 4; i++) {
+            for (int s = 4; s >= i; s--)
                 System.out.print(" ");
             {
-            for(int j = 1;j <= i;j++)
-                System.out.print("* ");
-            
-            System.out.println();
+                for (int j = 1; j <= i; j++)
+                    System.out.print("* ");
+
+                System.out.println();
             }
         }
     }
